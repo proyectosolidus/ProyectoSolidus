3f909d5ef1c9f6239278fb42a460167919201a2a
Gaspar Dolcemascolo <gaspardolcemascolo@gmail.com>
2021-02-09 09:16:22 -0300

    footer

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './styles/Footer.css'
+
+const Footer = () => {
+	return(
+		<footer className="Footer">
+			<p>Proyecto Solidus - 2021</p>
+		</footer>
+	)
+}
+
+export default Footer
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,12 +1,14 @@
 import React, {Fragment} from 'react'
 
 import Header from './Header'
+import Footer from './Footer'
 
 const Layout = props => {
 	return(
 		<div>
 			<Header />
 			{props.children}
+			<Footer />
 		</div>
 	)
 }
