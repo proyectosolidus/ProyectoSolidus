88b488601ec17713b49adc388c524a5dc6bc67f8
Gaspar Dolcemascolo <gaspardolcemascolo@gmail.com>
2021-02-09 09:56:55 -0300

    error 404 page

--- src/components/Error404.js
+++ src/components/Error404.js
@@ -1,7 +1,19 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+import Error404Image from '../images/error-404.svg'
+import './styles/Error404.css'
 
 const Error404 = () => {
 	return(
-		<h1>Error 404</h1>
+		<div className="box-wrapper">
+			<div className="Error404">
+				<div className="Error404__img">
+					<img src={Error404Image} />
+				</div>
+				<Link to="/" className="primary-btn">Volver al Home</Link>
+			</div>
+		</div>
 	)
 }
 
